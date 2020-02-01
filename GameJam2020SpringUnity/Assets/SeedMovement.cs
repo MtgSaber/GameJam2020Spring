ec74d6633e6b49e776253cb633e95df22429284b
Andrew Arnold <andrewja314@gmail.com>
2020-02-01 18:13:35 -0500

    Fixed seed movement finally

--- GameJam2020SpringUnity/Assets/SeedMovement.cs
+++ GameJam2020SpringUnity/Assets/SeedMovement.cs
@@ -9,6 +9,7 @@ public class SeedMovement : MonoBehaviour {
     
     private PlayerControllerScript playerScript;
     private Rigidbody2D rb;
+    private GameObject self;
     private Vector2 initialPosition;
     private int t;
 
@@ -18,16 +19,17 @@ public class SeedMovement : MonoBehaviour {
         rb = this.gameObject.GetComponentInChildren<Rigidbody2D>();
         this.initialPosition = this.rb.position;
         this.playerScript = GameObject.Find("Player").gameObject.GetComponent<PlayerControllerScript>();
+        if (this.type != 1) {
+            this.rb.gravityScale = 1f;
+            this.rb.velocity = new Vector2(this.direction, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (this.type) {
-            case 0:
-                MovementType1();
-                break;
-        }
+        if (this.type == 1)
+            MovementType1();
     }
 
     private void MovementType1() {
@@ -36,11 +38,11 @@ public class SeedMovement : MonoBehaviour {
     }
 
     private Vector2 MovementType1Func(int t) {
-        return new Vector2(-.2f * (1/60.0f * t - 3) * (1/60.0f * t - 3)  + 5,.2f * (3 - 1/60.0f*t));
+        return new Vector2(-this.direction * .3f * (1/60.0f * t - 3) * (1/60.0f * t - 3)  + 5,.3f * (3 - 1/60.0f*t));
     }
 
     public void OnTriggerEnter2D(Collider2D other) {
         this.playerScript.seedExists = false;
-        Destroy(this);
+        Destroy(this.gameObject);
     }
 }
