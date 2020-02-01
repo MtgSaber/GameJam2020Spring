5e1a8dc4eb4bb2056d3064dc1a127b86c9aa4121
Andrew Arnold <andrewja314@gmail.com>
2020-02-01 12:14:33 -0500

    Revert "Fixed movement inputs"
    This reverts commit ef4bb6cda2de24998086ea9e468d117c8e8d8ac9.

--- GameJam2020SpringUnity/Assets/PlayerControllerScript.cs
+++ GameJam2020SpringUnity/Assets/PlayerControllerScript.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +8,6 @@ public class PlayerControllerScript : MonoBehaviour
     private float vMove;
     private Vector2 movement;
     private bool canMove;
-    private bool isGrounded;
     private Rigidbody2D rb;
     public float speed;
     // Start is called before the first frame update
@@ -17,7 +15,6 @@ public class PlayerControllerScript : MonoBehaviour
     {
         canMove = true;
         rb = this.gameObject.GetComponentInChildren<Rigidbody2D>();
-        
     }
 
     // Update is called once per frame
@@ -25,35 +22,16 @@ public class PlayerControllerScript : MonoBehaviour
     {
         hMove = Input.GetAxis("Horizontal");
         vMove = Input.GetAxis("Vertical");
-        movement = new Vector2(hMove, (this.isGrounded ? this.vMove : 0)) * speed;
+        movement = new Vector2(hMove, vMove) * speed;
 
         Move();
     }
 
     void Move()
     {
-        if (canMove) {
-            Vector2 velocity = this.rb.velocity;
-            float projectedX = velocity.x + this.movement.x;
-            float projectedY = velocity.y + this.movement.y;
-            
-            /*
-            Debug.Log(
-                "Movement: {"
-                + "movement: " + this.movement.ToString()
-                + ", velocity: " + velocity.ToString()
-                + ", projectedX: " + projectedX
-                + ", projectedY" + projectedY
-                + "}"
-            );
-            */
-            
-            if (Math.Abs(projectedX) <= Math.Abs(this.speed))
-                velocity.x += this.movement.x;
-            if (Math.Abs(projectedY) <= Math.Abs(this.speed))
-                velocity.y += this.movement.y;
-
-            this.rb.velocity = velocity;
+        if (canMove)
+        {
+            rb.AddForce(movement);
         }
     }
 }
